cf52ebfccd46e435454beef8b9ce86f903fbafc0
Vsevolod Dolgopolov (aka Seva) <zavalit@googlemail.com>
2016-08-16 20:49:38 +0200

    sync

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,26 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+		$this->test = "test";
+		$this->test2 = "test2";
+		$this->test2 = "test3";
+    }
+}
